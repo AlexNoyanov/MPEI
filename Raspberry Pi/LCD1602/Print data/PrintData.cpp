0e88716ba209bd6f7d320c25c229920c32c364ed
RaspberryPiA <alex.noyanov@gmail.com>
2019-02-22 23:58:31 +0000

    Check PrintData to print data on lcd1602. Just print two strings!

--- Raspberry Pi/LCD1602/Print data/PrintData.cpp
+++ Raspberry Pi/LCD1602/Print data/PrintData.cpp
@@ -23,8 +23,17 @@
       GND | | 9/CE19           ->                  GND | | CE1
  */
 
-#include "PrintData.hpp"
+//#include "PrintData.hpp"
 
+#include <iostream>
+
+#include <wiringPi.h>
+#include <lcd.h>
+
+#include <unistd.h>
+
+#include <errno.h>
+#include <stdlib.h>
 
 const int RS = 3;       //
 const int EN = 14;      //
@@ -33,21 +42,37 @@ const int D1 = 12;      //
 const int D2 = 13;      //
 const int D3 = 6;       //
 
-#include <iostream>
-#include <wiringPi.h>
-#include <lcd.h>
 
+using namespace std;
 
-int main(int argc, char *argv[])
+int main(int argc,char *argv[])
 {
     // Experimenting with argc and *argv[] parameters
-    cout << "You have entered " << argc
-    << " arguments:" << "\n";
+    //cout << "You have entered " << argc
+    //<< " arguments:" << "\n";
     
-    for (int i = 0; i < argc; ++i)
-        cout << argv[i] << "\n";
+    //for (int i = 0; i < argc; ++i)
+        //cout << argv[i] << "\n";
 
+    
+    // Wiring pi status
+    if (wiringPiSetup() < 0)
+    {
+        //fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
+        return 1 ;
+    }
+    
+    int lcdFD;
+    lcdFD = lcdInit(2, 16, 4, RS, EN, D0, D1, D2, D3, D0, D1, D2, D3);
+    
+    lcdPosition(lcdFD, 0,0);
+    
+    lcdPrintf(lcdFD,argv[1]);
+    
+    lcdPosition(lcdFD, 0,1);
+    
+    lcdPrintf(lcdFD,argv[2]);
     
     return 0;
 }
-}
+
