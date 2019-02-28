9e5894af4a450a66b7f70b10811a5e729718c2cb
RaspberryPiA <alex.noyanov@gmail.com>
2019-02-28 20:23:20 +0000

    Script to talk with female vocie and some volume

---
+++ Raspberry Pi/Talking/femVoice.c
@@ -0,0 +1,19 @@
+
+
+// Program to talk with female voice
+// Using Festival speech sintheizer
+
+// Run this script with two parameters: <Phrase to talk> <volume>
+
+
+// December 28 2019 by ALex Noyanov
+
+#include<stdio.h>
+
+int main(int argc, char * argv[])
+{
+
+	system("");
+
+return 0;
+}
