20565a839abe39a2e6bb7f882c316722143a2292
Alex Noyanov <alex.noyanov@gmail.com>
2019-12-28 17:43:13 +0300

    Link to page "about" changed on the main page

--- Site/Printing messages LCD/printMessage.php
+++ Site/Printing messages LCD/printMessage.php
@@ -60,7 +60,7 @@
     <br><br><br>
     
     <!--  Button for About page -->
-    <input onclick="window.location.href = 'About.html' " type = 'submit' value = 'Learn more about this project' >
+    <input onclick="window.location.href = 'about.html' " type = 'submit' value = 'Learn more about this project' >
     </input><br>
     <br>
     <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
