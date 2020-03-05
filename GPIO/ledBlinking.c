0a9501cbac05ac7214a2727a8d52337c02b335a5
Alex Noyanov <alex.noyanov@gmail.com>
2020-03-05 19:25:39 +0300

    Added folders with photos examples

--- GPIO/ledBlinking.c
+++ GPIO/ledBlinking.c
@@ -1,6 +1,7 @@
 
 // Use this program to turn ON the GPIO pin
-// ledON <pin number>
+// ledBlinking <pin1 number> <pin2 number> <delay between blinks>
+// For example: ./ledBlinking 3 4 500
 
 #include <wiringPi.h>
 #include <stdio.h>
@@ -11,14 +12,14 @@ int pin = atoi(argv[1]);
 int pin2 = atoi(argv[2]);
 int delayTime = atoi(argv[3]);
 
- printf("Raspberry Pi wiringPi blink test\n");
+ printf(" ==== Raspberry Pi wiringPi blink test ====\n");
 
   if (wiringPiSetup() == -1)
-//    exit (1);
+  //    exit (1);
 
   pinMode(pin, OUTPUT);
 
-  for (;;){
+    while(1){
     printf("LED On\n");
     digitalWrite(pin, 1);
     digitalWrite(pin2,0);
@@ -27,7 +28,7 @@ int delayTime = atoi(argv[3]);
     digitalWrite(pin, 0);
     digitalWrite(pin2,1);
     delay(delayTime);
-  }
+    }
  return 0;
 
 }
